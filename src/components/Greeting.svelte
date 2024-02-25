de5322eac32da2aeaf06ee0bc6bd3ae58fba1440
tsantanal <tsantanal@ext.falabella.cl>
2024-02-24 21:11:37 -0500

    feat(components): create musics table

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    const currentTime = new Date()
+    const currentHour = currentTime.getHours()
+
+    const getGreeting = (currentHour: number) => {
+        if(currentHour < 12) return 'Good Morning';
+        if(currentHour < 18) return 'Good Afternoon';
+
+        return 'Good night';
+    } 
+
+    const greeting = getGreeting(currentHour);
+</script>
+
+<h1 class="text-3xl font-bold">
+    {greeting}
+</h1>
